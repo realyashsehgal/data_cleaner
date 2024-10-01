6f382390175721246433f0c61d19be7b7493b943
realyashsehgal <yash.sehgal1234@gmail.com>
2024-10-02 00:52:17 +0530

    new changes

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -1,10 +1,20 @@
 import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
 def clean_data(input_file, output_file):
     data = pd.read_csv(input_file)
     
     data.drop_duplicates(inplace=True)
-    
-    
-    data.fillna(data.mean(),inplace=True)
-    print("data claend")
+    missing_col = []
+    for column in data:
+        if data[column].dtype == np.int64 or np.float64 and data[column].isnull().any():
+            missing_col.append(column)
+            print("hell")
+            print(data[column])
+    print(missing_col)
+    for col in missing_col:
+        imputer = SimpleImputer(strategy='mean')
+        imputer.fit(data[[col]])
+        data[[col]] = imputer.transform(data[[col]])
+  
     data.to_csv(output_file,index= False)
