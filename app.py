a119cca7528fc0c6a428a267e65983a876fc0131
realyashsehgal <yash.sehgal1234@gmail.com>
2024-10-05 01:43:46 +0530

    adding show button with it minor bug where after clicking show upload wont work

--- app.py
+++ app.py
@@ -24,15 +24,29 @@ def upload():
 
     if file.filename == '':
         return redirect(request.url)  
-    
     file_path = os.path.join(app.config['UPLOAD_FILES'], file.filename)
     file.save(file_path)
-
-    cleaned_filepath = os.path.join(app.config['CLEANED_DATA'], f'cleaned_{file.filename}')
-    clean_data(file_path, cleaned_filepath) 
-
-    return redirect(url_for('result', filename=f'cleaned_{file.filename}'))
-
+    
+    button_clicked = request.form['action']
+    if button_clicked == 'upload':
+        
+        cleaned_filepath = os.path.join(app.config['CLEANED_DATA'], f'cleaned_{file.filename}')
+        print("upload clicked")
+        clean_data(file_path, cleaned_filepath) 
+        return redirect(url_for('result', filename=f'cleaned_{file.filename}'))
+    elif  button_clicked =='show':
+        print(file.filename)
+        df = pd.read_csv(file_path)
+        table_html = df.to_html(classes='table table-striped', index=False)
+       
+        file_path = os.path.join(app.config['UPLOAD_FILES'], file.filename)
+        file.save(file_path)
+        return render_template('upload.html', table_html=table_html,filename= file.filename) 
+    
+    button_clicked = request.form['action']
+    print(file.filename)
+    
+    return redirect(request.url) 
 @app.route('/result/<filename>')
 def result(filename):
     cleaned_filepath = os.path.join(app.config['CLEANED_DATA'], filename)
