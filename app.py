6acf5b4dfab8bb7a0647bf0e7058760fd62f981f
realyashsehgal <yash.sehgal1234@gmail.com>
2024-10-05 22:08:14 +0530

    major bug fix

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from src.data_cleaner import clean_data
 
 app = Flask(__name__, template_folder='templates', static_folder='static') 
 
-app.config['UPLOAD_FILES'] = 'data/raw'  
+app.config['UPLOAD_FILES'] = 'Data/raw'  
 app.config['CLEANED_DATA'] = 'Data/cleaned'
 
 os.makedirs(app.config['UPLOAD_FILES'], exist_ok=True)
 os.makedirs(app.config['CLEANED_DATA'], exist_ok=True)
-
+global global_file
 @app.route('/')
 def upload_file():
     return render_template('upload.html')
@@ -20,33 +20,43 @@ def upload():
     if 'file' not in request.files:
         return redirect(request.url) 
     
+    global file
     file = request.files['file']
-
     if file.filename == '':
         return redirect(request.url)  
+    
+    # print(global_file.filename)
+    global file_path
     file_path = os.path.join(app.config['UPLOAD_FILES'], file.filename)
     file.save(file_path)
+    # print(global_file)
     
     button_clicked = request.form['action']
     if button_clicked == 'upload':
-        
-        cleaned_filepath = os.path.join(app.config['CLEANED_DATA'], f'cleaned_{file.filename}')
-        print("upload clicked")
-        clean_data(file_path, cleaned_filepath) 
-        return redirect(url_for('result', filename=f'cleaned_{file.filename}'))
-    elif  button_clicked =='show':
-        print(file.filename)
-        df = pd.read_csv(file_path)
-        table_html = df.to_html(classes='table table-striped', index=False)
-       
-        file_path = os.path.join(app.config['UPLOAD_FILES'], file.filename)
-        file.save(file_path)
-        return render_template('upload.html', table_html=table_html,filename= file.filename) 
-    
-    button_clicked = request.form['action']
-    print(file.filename)
-    
+        return render_template('upload.html',filename=file.filename)
     return redirect(request.url) 
+
+@app.route('/display', methods=['POST'])
+def display():
+    # Ensure that the global_file is available
+    # if global_file or global_file.filename == '':
+    #     return redirect(url_for('upload'))  # Redirect if no file is uploaded
+
+    print("Display function invoked")
+    file_path = os.path.join(app.config['UPLOAD_FILES'], file.filename)
+    df = pd.read_csv(file_path)
+    table_html = df.to_html(classes='table table-striped', index=False)
+
+    return render_template('upload.html',table_html=table_html,filename=file.filename)
+
+
+@app.route('/clean', methods=['POST'])
+def clean():
+    cleaned_filepath = os.path.join(app.config['CLEANED_DATA'], f'cleaned_{file.filename}')
+    print("upload clicked")
+    clean_data(file_path, cleaned_filepath) 
+    return redirect(url_for('result', filename=f'cleaned_{file.filename}'))
+
 @app.route('/result/<filename>')
 def result(filename):
     cleaned_filepath = os.path.join(app.config['CLEANED_DATA'], filename)
