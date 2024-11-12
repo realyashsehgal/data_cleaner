f2b121b32a944db5cd38b9d58f3b16ec765969d8
realyashsehgal <yash.sehgal1234@gmail.com>
2024-11-12 21:26:51 +0530

    some major changes in ui and fucntioning

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import os
 import pandas as pd
-from src.data_cleaner import clean_data
+from src.data_cleaner import Handeling_null,Remove_duplicate,round_values
 import numpy as np
 
 app = Flask(__name__, template_folder='templates', static_folder='static') 
@@ -11,12 +11,18 @@ app.config['CLEANED_DATA'] = 'Data/cleaned'
 
 os.makedirs(app.config['UPLOAD_FILES'], exist_ok=True)
 os.makedirs(app.config['CLEANED_DATA'], exist_ok=True)
-global global_file
-global numeric_col
-numeric_col = []
+
+
+
+
+
+
+
 @app.route('/')
 def upload_file():
     return render_template('upload.html')
+
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -24,36 +30,73 @@ def upload():
     if 'file' not in request.files:
         print("File not found")
         return redirect(request.url) 
-    
     global file
     file = request.files['file']
     if file.filename == '':
         return redirect(request.url)  
-    
     global file_path
     file_path = os.path.join(app.config['UPLOAD_FILES'], file.filename)
     file.save(file_path)
+    print(file)
+    
+    
+    
+    global cleaned_filepath
+    cleaned_filepath = os.path.join(app.config['CLEANED_DATA'], f'cleaned_{file.filename}')
+    # Initially, save a copy of the uploaded file as the base "cleaned" file.
+    # file.save(cleaned_filepath)
+    
+    
     
     button_clicked = request.form['action']
     if button_clicked == 'upload':
-        return render_template('upload.html',filename=file.filename)
+        return render_template('work.html',filename=file.filename)
     return redirect(request.url) 
+
+
+
+
 
 @app.route('/display', methods=['POST'])
 def display():
     file_path = os.path.join(app.config['UPLOAD_FILES'], file.filename)
     df = pd.read_csv(file_path)
     table_html = df.to_html(classes='table table-striped', index=False)
-    return render_template('upload.html',table_html=table_html,filename=file.filename)
+    return render_template('work.html',table_html=table_html,filename=file.filename)
 
 
     
+
+
+
+@app.route('/Handeling_null_values', methods=['POST'])
+def Handeling_null_values():
+    Handeling_null(cleaned_filepath)
+    # return redirect(url_for('result', filename=f'cleaned_{file.filename}'))
+    return render_template('work.html',filename=file.filename)
+
+
+
+@app.route('/remove_duplicate',methods=['POST'])
+def remove_duplicate():
+    Remove_duplicate(cleaned_filepath)
+    # return redirect(url_for('result', filename=f'cleaned_{file.filename}'))
+    return render_template('work.html',filename=file.filename)
+    
+
+@app.route('/Round',methods=['POST'])
+def Round():
+    round_values(cleaned_filepath)
+    return render_template('work.html',filename=file.filename)
+
+
 @app.route('/clean', methods=['POST'])
 def clean():
-    cleaned_filepath = os.path.join(app.config['CLEANED_DATA'], f'cleaned_{file.filename}')
-    clean_data(file_path, cleaned_filepath)
-    df = pd.read_csv(file_path)
+    print(file.filename)
     return redirect(url_for('result', filename=f'cleaned_{file.filename}'))
+
+
+
 
 @app.route('/result/<filename>')
 def result(filename):
@@ -73,6 +116,15 @@ def result(filename):
     table_html = df.to_html(classes='table table-striped', index=False)
 
     return render_template('result.html', table_html=table_html, filename=filename, unique_values=unique_values)
+
+
+
+@app.route('/preview',methods=['POST'])
+def preview():
+    # file_path = os.path.join(app.config['CLEANED_DATA'], file.filename)
+    df = pd.read_csv(cleaned_filepath)
+    table_html = df.to_html(classes='table table-striped', index=False)
+    return render_template('work.html',table_html=table_html)
 
 
 
--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
-def clean_data(input_file, output_file):
-    data = pd.read_csv(input_file)
-    
-    data.drop_duplicates(inplace=True)
+
+data = pd.read_csv('Data\\raw\\dup.csv')
+data.dtypes
+def Handeling_null(output_file):
+    data = pd.read_csv(output_file)
     missing_col = []
     for column in data:
         if data[column].dtype == np.number and data[column].isnull().any():
             missing_col.append(column)
+        else:
+             data = data[data[column].notna()]
     print(missing_col)
     for col in missing_col:
         imputer = SimpleImputer(strategy='mean')
@@ -16,3 +19,19 @@ def clean_data(input_file, output_file):
         data[[col]] = imputer.transform(data[[col]])
   
     data.to_csv(output_file,index= False)
+
+
+
+def Remove_duplicate(output_file):
+    data = pd.read_csv(output_file)
+    data.drop_duplicates(inplace=True)
+    data.to_csv(output_file,index= False)
+    
+    
+    
+def round_values(output_file):
+    data = pd.read_csv(output_file)
+    for var in data:
+        if data[var].dtype == np.number:
+            round(data[var],2)
+    data.to_csv(output_file,index=False)
